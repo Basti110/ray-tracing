8c78e70e563ff8022d54f50e53c92a750561d9ab
Sebastian Preuß <sebastian.preuss@rwth-aachen.de>
2018-12-02 16:49:11 +0100

    added plane

---
+++ src/light.rs
@@ -0,0 +1,78 @@
+use crate::{Node, Ray, Color};
+use std::rc::{Weak, Rc};
+use cgmath::{Matrix4, Vector3};
+use std::cell::RefCell;
+
+pub struct Light {
+    childs: Vec<Rc<RefCell<Node>>>,
+    parent: Weak<Rc<RefCell<Node>>>,
+    size: usize,
+    pub name: String,
+    pub frame_transform: Matrix4<f64>,
+    pub direction: Vector3<f64>,
+    pub color: Color,
+    pub intensity: f32,
+}
+
+impl Light {
+    pub fn new(name: String, transform: Matrix4<f64>, direction: Vector3<f64>, color: Color, intensity: f32) -> Light {
+        Light {
+            childs: vec![],
+            parent: Weak::new(),
+            size: 0,
+            name: name,
+            frame_transform: transform,
+            direction: direction,
+            color: color,
+            intensity: intensity,
+        }
+    }
+
+    pub fn off_light() -> Light {
+        Light {
+            childs: vec![],
+            parent: Weak::new(),
+            size: 0,
+            name: "empty".to_string(),
+            frame_transform: Matrix4::from_translation(Vector3::new(0.0, 0.0, -5.0)),
+            direction: Vector3::new(0.0, 0.0, 1.0),
+            color: Color::new_rgb(0, 0, 0),
+            intensity: 0.0,
+        }
+    }
+}
+
+impl Node for Light {
+    fn get_parent(&self) -> Option<Rc<RefCell<Node>>> {
+        let strong = &self.parent.upgrade();
+        let strong = match strong {
+            Some(x) => x,
+            None => return None,
+        };
+        return Some(Rc::clone(&(*strong))); //Some(Rc::clone(&(*(&self.parent))));
+    }
+
+    fn get_child(&self, index: usize) -> Option<Rc<RefCell<Node>>> {
+        if index >= self.size {
+            return None;
+        }
+        return Some(Rc::clone(&(self.childs[index])));
+    }
+
+    fn add_child(&mut self, node: Rc<RefCell<Node>>) {
+        self.size += 1;
+        self.childs.push(Rc::clone(&node));
+    }
+
+    fn get_size(&self) -> usize {
+        return self.size;
+    }
+
+    fn intersect(&self, ray: &Ray) -> Option<(f64, Vector3<f64>)> {
+        return None;
+    }
+
+    fn get_color(&self) -> Color {
+        return self.color.copy();
+    }
+}
---
+++ src/plane.rs
@@ -0,0 +1,79 @@
+use crate::{Node, Ray, Color};
+use std::rc::{Weak, Rc};
+use cgmath::{InnerSpace, Matrix4, Vector3, Point3, EuclideanSpace};
+use std::cell::RefCell;
+
+pub struct Plane {
+    childs: Vec<Rc<RefCell<Node>>>,
+    parent: Weak<Rc<RefCell<Node>>>,
+    size: usize,
+    pub name: String,
+    pub frame_transform: Matrix4<f64>,
+    pub origin: Point3<f64>,
+    pub normal: Vector3<f64>,
+    pub color: Color,
+}
+
+impl Plane {
+    pub fn new(name: String, transform: Matrix4<f64>, origin: Point3<f64>, normal: Vector3<f64>, color: Color) -> Plane {
+        Plane {
+            childs: vec![],
+            parent: Weak::new(),
+            size: 0,
+            name: name,
+            frame_transform: transform,
+            origin: origin,
+            normal: normal,
+            color: color,
+        }
+    }
+}
+
+impl Node for Plane {
+    fn get_parent(&self) -> Option<Rc<RefCell<Node>>> {
+        let strong = &self.parent.upgrade();
+        let strong = match strong {
+            Some(x) => x,
+            None => return None,
+        };
+        return Some(Rc::clone(&(*strong))); //Some(Rc::clone(&(*(&self.parent))));
+    }
+
+    fn get_child(&self, index: usize) -> Option<Rc<RefCell<Node>>> {
+        if index >= self.size {
+            return None;
+        }
+        return Some(Rc::clone(&(self.childs[index])));
+    }
+
+    fn add_child(&mut self, node: Rc<RefCell<Node>>) {
+        self.size += 1;
+        self.childs.push(Rc::clone(&node));
+    }
+
+    fn get_size(&self) -> usize {
+        return self.size;
+    }
+
+    fn intersect(&self, ray: &Ray) -> Option<(f64, Vector3<f64>)> {
+        let normal = self.normal;
+        let denom = normal.dot(ray.direction);
+        if denom > 1e-6 {    
+            let v = self.origin - ray.origin;
+            let distance = v.dot(normal) / denom;
+            if distance >= 0.0 {
+                let hit_point = ray.origin + (ray.direction * distance);
+                let l = hit_point.to_vec() - self.origin.to_vec();
+                //let l = l.magnitude();
+                if l.x < 4.0 && l.y < 4.0 && l.z < 4.0 {
+                    return Some((distance, normal));
+                }
+            }
+        }
+        None
+    }
+
+    fn get_color(&self) -> Color {
+        return self.color.copy();
+    }
+}
